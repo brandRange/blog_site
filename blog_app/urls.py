9080e12cf961343324902804f4020427f035c888
Kofi Brandful <rhutaford@gmail.com>
2025-09-19 17:12:53 +0000

    added individual view page

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_details
 
 urlpatterns = [
-    path("",post_list, name="home")
+    path("",post_list, name="home"),
+    path("post/<int:pk>/", post_details, name="post_detail")
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
 from .models import Post
@@ -6,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all()
     return render(request, "home.html",  {"posts":posts})
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_detail.html", {"post": post})
